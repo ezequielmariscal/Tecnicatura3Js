f39b943dfe1e4306eac3e97d76c8052692939aed
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-04-28 23:39:26 -0300

    Arrancando leccion03 js UTN

---
+++ TSLeccion03/10-03-funciones.js
@@ -0,0 +1,9 @@
+function miFuncion(a, b){
+    //console.log("Sumamos: "+ (a+b));
+    return a+b;
+}
+//Llamamos la funcion
+miFuncion(5,4);
+
+let resultado = miFuncion(6, 7);
+console.log(resultado);
