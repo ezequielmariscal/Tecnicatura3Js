97422f924ece52b0acf3d0a788f23906b5ab71e2
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-05-06 13:54:36 -0300

    Add_forIn_Add_Supr

--- TSLeccion04/01-04-objetos.js
+++ TSLeccion04/01-04-objetos.js
@@ -22,4 +22,36 @@ console.log(persona); // aca accedo a todo
 
 // Agregar un nuevo metodo a un objeto (ver linea 11)
 // como accedo?
+
 console.log(persona.nombreCompleto()); // Carlos Gil
+
+// Creamos un nuevo object
+
+let persona2 = new Object(); // Debe crear un nuevo objeto en memoria
+persona2.nombre = 'Juan';
+persona2.direccion = 'Salada 14';
+persona2.telefono = '03030456';
+console.log(persona2.telefono);
+
+// Como acceder a las propiedades de los object desde otra manera?
+
+console.log(persona['apellido']); // Accedemos como si fuera un Array
+
+// for in
+
+for(propiedad in persona){
+    console.log(propiedad);
+    console.log(persona[propiedad]);
+}
+
+// Agregar o eliminar object
+
+//Agregar
+
+persona.apellido = 'Betancud'; // cambiamos dinamicamente el valor de un object
+console.log(persona);
+
+//Eliminar
+
+persona.apellida = 'benntacud';
+delete persona.apellida; // Elimina lo que por error creamos
