535d2a9f93c75f7073a57a63852de180abc3a779
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-05-15 22:32:14 -0300

    Add_method_GET

--- TSLeccion05/05-03clases.js
+++ TSLeccion05/05-03clases.js
@@ -1,11 +1,19 @@
 class Persona{
     constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this._nombre = nombre;
+        this._apellido = apellido;
     }
+
+    // Add method get (Los atributos no pueden llamarse igual cuando creamos metodo get y set)
+    get nombre(){
+        return this._nombre;
+    }
+
+    // Add method set
 }
 
 let persona1 = new Persona('Martin', 'Fierro');
-console.log(persona1);
-let persona2 = new Persona('Carlos', 'Baute');
-console.log(persona2);
+console.log(persona1.nombre);
+//console.log(persona1);
+console.log(persona2.nombre);
+//console.log(persona2);
--- TSLeccion05/README.md
+++ TSLeccion05/README.md
@@ -1,3 +1,11 @@
 # Constructor en JS
 
 ## En JavaScript, un constructor es una función especial que se utiliza para crear e inicializar objetos. Los constructores se definen usando la sintaxis de función y se invocan utilizando el operador "new".
+
+# Metodo GET y SET
+
+## Los atributos no pueden llamarse igual cuando creamos metodo get y set
+
+# GET
+
+## En JavaScript, puedes definir un método "get" en una clase o en un objeto utilizando la sintaxis de acceso de propiedad. Un método "get" permite acceder a una propiedad de un objeto como si fuera una propiedad directa, pero en realidad se ejecuta una función que devuelve el valor deseado.
