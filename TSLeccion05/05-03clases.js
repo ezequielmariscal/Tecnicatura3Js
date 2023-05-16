c4d723ddaa05552d38aa12a82fdc09c65002b49c
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-05-15 22:25:02 -0300

    Viendo Constructores en js

---
+++ TSLeccion05/05-03clases.js
@@ -0,0 +1,11 @@
+class Persona{
+    constructor(nombre, apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+let persona1 = new Persona('Martin', 'Fierro');
+console.log(persona1);
+let persona2 = new Persona('Carlos', 'Baute');
+console.log(persona2);
---
+++ TSLeccion05/README.md
@@ -0,0 +1,3 @@
+# Constructor en JS
+
+## En JavaScript, un constructor es una función especial que se utiliza para crear e inicializar objetos. Los constructores se definen usando la sintaxis de función y se invocan utilizando el operador "new".
