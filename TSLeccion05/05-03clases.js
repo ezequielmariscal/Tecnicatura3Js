fafa26235b202c3468633b26e3c52b8506bbb07e
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-05-17 22:22:57 -0300

    Add_tarea

--- TSLeccion05/05-03clases.js
+++ TSLeccion05/05-03clases.js
@@ -13,6 +13,15 @@ class Persona{
     set nombre(nombre){
         this._nombre = nombre;
     }
+
+     // TAREA.....Add method get (Los atributos no pueden llamarse igual cuando creamos metodo get y set)
+     get apellido(){
+        return this._apellido;
+    }
+     //  TAREA .....Add method set
+     set apellido(apellido){
+        this._apellido = apellido;
+    }
 }
 
 let persona1 = new Persona('Martin', 'Fierro');
@@ -24,3 +33,12 @@ console.log(persona2.nombre);
 persona2.nombre = 'Maria Laura'; // set
 console.log(persona2.nombre);
 //console.log(persona2);
+
+/* Tarea: Crear el método get y set para el atributo de apellido, 
+luego modificar el apellido a través del  set y mostrar con un 
+console.log lo que es el get donde muestra el resultado obtenido. */
+
+let persona3 = new Persona('Pepe, Argento');
+console.log(persona3.apellido);
+persona3.apellido = ('Mujica');
+console.log(persona3.apellido);
