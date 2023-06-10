06f7cd75de90eafb1579a714a605452cd902ee2c
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-06-10 19:44:26 -0300

    Add_class9_part1.1

---
+++ TSleccion07/Persona.js
@@ -0,0 +1,11 @@
+class Persona{
+    
+    static contadorPersona = 0;
+
+    constructor(nombre, apellido, edad){
+        this._idPersona = ++Persona.contadorPersonas;
+        this._nombre = nombre;
+        this._apellido = apellido;
+        this._edad = edad;
+    }
+}
