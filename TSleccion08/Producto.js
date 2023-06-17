7082bc8c725cfaaa44d92d3394878241ec05f40b
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-06-17 17:41:42 -0300

    Add_class10

--- TSleccion08/Producto.js
+++ TSleccion08/Producto.js
@@ -1,4 +1,4 @@
-class Producto extends Orden{
+class Producto {
     
     static contadorProductos = 0;
 
@@ -8,7 +8,31 @@ class Producto extends Orden{
         this._precio = precio;
     }
 
+    /*
+
     idProducto = int;
     nombre = String;
     precio = int;
+    */
+    get _idProducto(){
+        return this._idProducto;
+    }
+    get nombre(){
+        return this._nombre;
+    }
+    // Add method set
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+     get precio(){
+        return this._precio;
+    }
+     //  Add method set
+     set precio(precio){
+        this._precio = precio;
+    }
+
+    toString(){
+        return `${this._idProducto} ${this._nombre} ${this._precio}`;
+    }
 }
