65280966f43f7250757778e54a1ebf5528ae42d6
ezequielmariscal <85526762+ezequielmariscal@users.noreply.github.com>
2023-06-18 00:33:12 -0300

    Add_method_Orden

--- TSleccion08/Orden.js
+++ TSleccion08/Orden.js
@@ -7,20 +7,28 @@ class Orden{
     }
 
     constructor(){ 
-        this._idOrden =++Orden.contadorOrdenes;
-        super(Producto, nombre, precio);
-        
+        this._idOrden = ++Orden.contadorOrdenes;
+        this._productos = []; // Recibe cada elemnto de productos
+        this._contadorProductosAgregado = 0;
 
     }
 
-    Orden(){
+    get idOrden(){
+        return this._idOrden
+    }
 
-    };
-    agregarProducto(Producto);
+    agregarProducto(Producto){
+        if(this._productos.length < Orden.MAX_PRODUCTO){
+            this._productos.push(Producto); // Tnemos 2 tipos de sintaxis : 1
+            //this._productos[this._contadorProductosAgregado++] = Producto; ----SINTAXIS 2
+        }else{
+            console.log('No se pueden agregar mas productos');
+        }
+    }// Fin del m,etodo agregarProducrto
     CalcularTotal(){
         Producto = id_Producto;
     };
     mostrarOrden(){
-        
+
     };
-}
+} // Fin de la clase orden
